cb3398907725ac19883d0ea7de9eeeed548550d0
Giovanni <kreissagellc@gmail.com>
2019-10-25 23:05:15 -0600

    Update #1 program.py
    +pin.DOOR_SENSOR_PIN = 18

--- program.py
+++ program.py
@@ -5,7 +5,8 @@ import signal
 # Set Broadcom mode so we can address GPIO pins by number.
 GPIO.setmode(GPIO.BCM) 
 # This is the GPIO pin number we have one of the door sensor
-# wires attached to, the other should be attached to a ground pin.DOOR_SENSOR_PIN = 18
+# wires attached to, the other should be attached to a ground 
+pin.DOOR_SENSOR_PIN = 18
 # These are the GPIO pin numbers we have the lights attached to
 RED_LIGHT = 9
 YELLOW_LIGHT = 10
